79052cae4ed0812cb323d0abfaec541a43dd6874
Joel Van Horn <joel@joelvanhorn.com>
2017-09-13 22:15:46 -0400

    Refactored for clarity

--- lib/omniauth-spotify.rb
+++ lib/omniauth-spotify.rb
@@ -6,16 +6,18 @@ module OmniAuth
     class Spotify < OmniAuth::Strategies::OAuth2
       # Give your strategy a name.
       option :name, 'spotify'
-      
+
       FORCE_APPROVAL_KEY = 'ommiauth_spotify_force_approval?'.freeze
 
       # This is where you pass the options you would pass when
       # initializing your consumer from the OAuth gem.
       option :client_options, {
-        :site          => 'https://api.spotify.com/v1',
-        :authorize_url => 'https://accounts.spotify.com/authorize',
-        :token_url     => 'https://accounts.spotify.com/api/token',
+        site:          'https://api.spotify.com/v1',
+        authorize_url: 'https://accounts.spotify.com/authorize',
+        token_url:     'https://accounts.spotify.com/api/token',
       }
+
+      option :authorize_options, [:scope, :show_dialog]
 
       # These are called after authentication has succeeded. If
       # possible, you should try to set the UID without making
@@ -30,61 +32,59 @@ module OmniAuth
           # and the name field will be set to the username/nickname instead of the display name.
           # The email field will be nil if the 'user-read-email' scope isn't included.
           #
-          :name => raw_info['display_name'] || raw_info['id'],
-          :nickname => raw_info['id'],
-          :email => raw_info['email'],
-          :urls => raw_info['external_urls'],
-          :image => image_url,
-          :birthdate => raw_info['birthdate'] && Date.parse(raw_info['birthdate']),
-          :country_code => raw_info['country'],
-          :product => raw_info['product'],
-          :follower_count => raw_info['followers']['total']
+          name:     raw_info['display_name'] || raw_info['id'],
+          nickname: raw_info['id'],
+          email:    raw_info['email'],
+          urls:     raw_info['external_urls'],
+          image:    image_url,
+          location: raw_info['country'],
+
+          # non-standard
+
+          birthdate:      raw_info['birthdate'] && Date.parse(raw_info['birthdate']),
+          country_code:   raw_info['country'],
+          product:        raw_info['product'],
+          follower_count: raw_info['followers']['total']
         }
       end
 
       extra do
         {
-          'raw_info' => raw_info
+          raw_info: raw_info
         }
       end
 
       def image_url
-        if images = raw_info['images']
-          if first = images.first
-            first['url']
-          end
-        end
+        image = Array(raw_info['images']).first
+        image['url'] if image
       end
 
       def raw_info
         @raw_info ||= access_token.get('me').parsed
       end
-      
+
       def authorize_params
         super.tap do |params|
-          if session.delete(FORCE_APPROVAL_KEY) ||
-             (session[:flash] && session[:flash]['flashes'] && session[:flash]['flashes'][FORCE_APPROVAL_KEY])
+          if session.delete(FORCE_APPROVAL_KEY) || session.fetch(:flash, {}).fetch('flashes', {})[FORCE_APPROVAL_KEY]
             params[:show_dialog] = true
           end
         end
       end
 
       def request_phase
-        %w[show_dialog].each do |v|
-          if request.params[v]
-            options[:authorize_params][v.to_sym] = request.params[v]
-          end
+        %i[show_dialog].each do |key|
+          options[:authorize_params][key] = request.params[key] if request.params[key]
         end
+
         super
       end
 
       def callback_url
-        if @authorization_code_from_signed_request_in_cookie
-          ''
-        else
-          # Fixes regression in omniauth-oauth2 v1.4.0 by https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
-          options[:callback_url] || (full_host + script_name + callback_path)
-        end
+        return '' if @authorization_code_from_signed_request_in_cookie
+
+        # Fixes regression in omniauth-oauth2 v1.4.0
+        # See: https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
+        options[:callback_url] || (full_host + script_name + callback_path)
       end
     end
   end
